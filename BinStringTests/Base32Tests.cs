6529188672f1992eed29260eb0e07f102c5bc0a6
Miff Falden <miff@miffthefox.info>
2017-10-09 12:17:14 -0400

    Add Base32

---
+++ BinStringTests/Base32Tests.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MiffTheFox;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinStringTests
+{
+    [TestClass]
+    public class Base32Tests
+    {
+        [TestMethod]
+        public void Base32EncodingTest()
+        {
+            var base32 = new Base32();
+            var hello = BinString.FromTextString("Hello, world!", Encoding.ASCII);
+
+            // null/empty
+            Assert.AreEqual(string.Empty, base32.GetString(BinString.Empty));
+            Assert.AreEqual(string.Empty, base32.GetString(null));
+
+            // basic
+            Assert.AreEqual("JBSWY3DPFQQHO33SNRSCC===", base32.GetString(hello));
+            Assert.AreEqual("KRUGS4ZANFZSAYJAORSXG5BO", base32.GetString(BinString.FromTextString("This is a test.", Encoding.UTF8)));
+            
+            // custom/no padding
+            base32.Padding = '?';
+            Assert.AreEqual("ME??????", base32.GetString((BinString)97));
+            base32.UsePadding = false;
+            Assert.AreEqual("JBSWY3DPFQQHO33SNRSCC", base32.GetString(hello));
+
+            // alternate character set
+            var crockfordBase32 = new Base32(Base32.CHARSET_CROCKFORD.ToLowerInvariant()) { UsePadding = false };
+            Assert.AreEqual("91jprv3f5gg7evvjdhj22", crockfordBase32.GetString(hello));
+        }
+
+        [TestMethod]
+        public void Base32DecodingTest()
+        {
+            var base32 = new Base32() { Culture = CultureInfo.InvariantCulture };
+
+            // null/empty
+            Assert.AreEqual(BinString.Empty, base32.GetBinString(null));
+            Assert.AreEqual(BinString.Empty, base32.GetBinString(string.Empty));
+            Assert.AreEqual(BinString.Empty, base32.GetBinString("========"));
+
+            // basic
+            Assert.AreEqual("vwxyz", base32.GetBinString("OZ3XQ6L2").ToString(Encoding.ASCII));
+            Assert.AreEqual("Decode", base32.GetBinString("IRSWG33EMU======").ToString(Encoding.ASCII));
+            Assert.AreEqual("==TEST==", base32.GetBinString("HU6VIRKTKQ6T2===").ToString(Encoding.ASCII));
+
+            // without padding
+            Assert.AreEqual("~test~", base32.GetBinString("PZ2GK43UPY").ToString(Encoding.ASCII));
+            Assert.AreEqual("?", base32.GetBinString("H4").ToString(Encoding.ASCII));
+
+            // white space, lower case
+            Assert.AreEqual("WhiteSpace", base32.GetBinString("\r\nK5UG S5DF KNYG\tCY\t3F\n").ToString(Encoding.ASCII));
+            Assert.AreEqual("~?~", base32.GetBinString("py7x4===").ToString(Encoding.ASCII));
+
+            // invalid characters
+            Assert.ThrowsException<FormatException>(() => base32.GetBinString("ERROR!"));
+            base32.IgnoreCase = false;
+            Assert.ThrowsException<FormatException>(() => base32.GetBinString("py7x4==="));
+
+            // alternate character set
+            var crockfordBase32 = new Base32(Base32.CHARSET_CROCKFORD.ToLowerInvariant());
+            Assert.AreEqual("12345", crockfordBase32.GetBinString("64s36d1n").ToString(Encoding.ASCII));
+        }
+
+        [TestMethod]
+        public void RoundTripTest()
+        {
+            var data = BinString.Empty;
+            var base32 = new Base32(Base32.CHARSET_ZBASE32);
+
+            for (int i = 0; i < 100; i++)
+            {
+                Assert.AreEqual(data, base32.GetBinString(base32.GetString(data)));
+                data += Convert.ToByte((i * 3) % 256);
+            }
+        }
+    }
+}
---
+++ MiffTheFox.BinString/Base32.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiffTheFox
+{
+    public class Base32
+    {
+        public const string CHARSET_RFC4648 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
+        public const string CHARSET_ZBASE32 = "ybndrfg8ejkmcpqxot1uwisza345h769";
+        public const string CHARSET_CROCKFORD = "0123456789ABCDEFGHJKMNPQRSTVWXYZ";
+        public const string CHARSET_BASE32HEX = "0123456789ABCDEFGHIJKLMNOPQRSTUV";
+
+        private string _CharacterSet;
+        public string CharacterSet
+        {
+            get => _CharacterSet;
+            set
+            {
+                if (value.Length != 32)
+                {
+                    throw new InvalidOperationException("Character set must be exactly 32 characters.");
+                }
+                else
+                {
+                    _CharacterSet = value;
+                }
+            }
+        }
+
+        public char Padding { get; set; } = '=';
+        public bool UsePadding { get; set; } = true;
+        public bool IgnoreCase { get; set; } = true;
+        public CultureInfo Culture { get; set; }
+        
+        public Base32(string characterSet = CHARSET_RFC4648)
+        {
+            CharacterSet = characterSet;
+            Culture = CultureInfo.CurrentCulture;
+        }
+
+        public override string ToString()
+        {
+            return _CharacterSet;
+        }
+
+        public string GetString(BinString data)
+        {
+            if (BinString.IsNullOrEmpty(data)) return string.Empty;
+
+            int building = 0;
+            int buildLocation = 4;
+            var result = new StringBuilder();
+            
+            foreach (byte b in data)
+            {
+                for (int i = 7; i >= 0; i--)
+                {
+                    building |= ((b >> i) & 1) << buildLocation;
+                    buildLocation--;
+
+                    if (buildLocation < 0)
+                    {
+                        result.Append(_CharacterSet[building]);
+                        building = 0;
+                        buildLocation = 4;
+                    }
+                }
+            }
+            
+            if (buildLocation < 4)
+            {
+                result.Append(_CharacterSet[building]);
+            }
+
+            if (UsePadding)
+            {
+                while (result.Length % 8 != 0)
+                {
+                    result.Append(Padding);
+                }
+            }
+
+            return result.ToString();
+        }
+
+        public BinString GetBinString(string encoded)
+        {
+            return GetBinString(encoded, Culture);
+        }
+
+        public BinString GetBinString(string encoded, CultureInfo culture)
+        {
+            if (string.IsNullOrEmpty(encoded)) return BinString.Empty;
+            string chars = _CharacterSet;
+
+            if (IgnoreCase)
+            {
+                encoded = encoded.ToUpper(culture).TrimEnd(char.ToUpper(Padding, culture));
+                chars = chars.ToUpper(culture);
+            }
+            else
+            {
+                encoded = encoded.TrimEnd(Padding);
+            }
+
+            var intData = encoded.Where(c => !char.IsWhiteSpace(c)).Select(c => chars.IndexOf(c)).ToArray();
+            if (intData.Any(c => c == -1)) throw new FormatException("This string contains one or more characters not allowed in the character set.");
+            int expectedLength = (intData.Length * 5) / 8;
+
+            int building = 0;
+            int buildLocation = 7;
+            using (var result = new BinStringBuilder())
+            {
+                foreach (int encodedInt in intData)
+                {
+                    for (int i = 4; i >= 0; i--)
+                    {
+                        building |= ((encodedInt >> i) & 1) << buildLocation;
+                        buildLocation--;
+
+                        if (buildLocation < 0)
+                        {
+                            result.Append(Convert.ToByte(building));
+                            building = 0;
+                            buildLocation = 7;
+                        }
+                    }
+                }
+
+                if (buildLocation < 7)
+                {
+                    result.Append(Convert.ToByte(building));
+                }
+
+                var decoded = result.ToBinString();
+                if (decoded.Length > expectedLength)
+                {
+                    return decoded.Substring(0, expectedLength);
+                } else
+                {
+                    return decoded;
+                }
+            }
+        }
+    }
+}
