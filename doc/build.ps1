e7d43b3e23e199d792008297fcafc068954d921a
Miff <miff@miffthefox.info>
2020-01-24 12:35:31 -0500

    add images

--- doc/build.ps1
+++ doc/build.ps1
@@ -8,15 +8,21 @@ $base = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
 # as documentation and page contents from a cache in the directory instead of
 # re-reading it from the source files. We have to remove the obj directory to
 # force it to do a clean build.
+# Let's clean up _site while we're at it so we do a clean build every time.
 Remove-Item -Recurse "$base\obj" -ErrorAction SilentlyContinue
+Remove-Item -Recurse "$base\_site" -ErrorAction SilentlyContinue
 
 # Let's actually build the damn thing.
 & docfx "$base\docfx.json"
 
 # DocFX AS USUAL is ignoring the templates specified in the JSON file.
-# This software is complete awful, but let's just hack up the CSS is makes
+# This software is complete awful, but let's just hack up the CSS it makes
 # to add a glorious dark theme.
 Add-Content "$base\_site\styles\main.css" -Value (Get-Content "$base\templates\darkfx\styles\main.css")
+
+# I have no idea where the favicon is supposed to go tbh. As long as this build
+# script is here, let's just throw it into _site.
+Copy-Item "$base\images\favicon.ico" "$base\_site\favicon.ico" -Force
 
 # Becuase JavaScript is all the rage with the kids these days, DocFX uses
 # AJAX to load content, despite the fact that it's generationg what's
